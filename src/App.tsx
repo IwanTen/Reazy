d6a28d99632d58d86321d93f7b501bf8fc068331
Iwan <iwantraegerpayne@gmail.com>
2022-07-08 22:07:38 -0400

    updated styling
    adding a new theme provider that uses css variables.
    -moved theme buttons to footer and added transition animation.
    -added name credit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
-import { bodyTheme, headerTheme, buttonTheme } from "./themeStyles";
-import "./App.css";
+import "./styles/App.css";
 
+enum THEME_NAMES {
+  STRAWBERRY = "strawberry",
+  CHOCOLATE = "chocolate",
+  MINT = "mint",
+}
 const startText =
-  "Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content.";
+  "Space Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic Reading aims to encourage a more in-depth reading and understanding of written content.";
 
 //TODO
 //create function that selects a portion of the text based on string length and wraps it in a div.
 function App() {
-  const [textArray, setTextArray] = useState<string[]>(startText.split(" "));
+  const [textArray, setTextArray] = useState<string[]>(
+    startText.split(/(\s+)/)
+  );
   const [inputText, setInputText] = useState<string>("");
-  const [theme, setTheme] = useState("chocolate");
-  useEffect(() => console.log(inputText));
+  const [theme, setTheme] = useState(THEME_NAMES.CHOCOLATE);
+  // useEffect(() => console.log(inputText));
 
   const handleInput = (event: any) => {
     let { value } = event.target;
@@ -20,67 +26,59 @@ function App() {
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    if (inputText.length > 0) setTextArray(inputText.split(" "));
+    if (inputText.length > 0) setTextArray(inputText.split(/(\s+)/));
   };
 
   let outputText = textArray.map((val) => {
-    let n = val.length === 1 ? 1 : val.length / 2;
-    let highlight = val.substring(0, n);
-    let textEnd = val.substring(n);
-    return (
-      <>
-        <span className="textBold">{highlight}</span>
-        <span>{textEnd + " "}</span>
-      </>
-    );
+    if (val[0] != " ") {
+      let n = val.length === 1 ? 1 : val.length / 2;
+      let highlight = val.substring(0, n);
+      let textEnd = val.substring(n);
+      return (
+        <>
+          <span className="textBold">{highlight}</span>
+          {textEnd}
+        </>
+      );
+    } else return val;
   });
 
-  const setThemeStrawberry: any = () => {
-    setTheme("strawberry");
-  };
-  const setThemeMint: any = () => {
-    setTheme("mint");
-  };
-  const setThemeChocolate: any = () => {
-    setTheme("chocolate");
-  };
-  const setThemeBoba: any = () => {
-    setTheme("boba");
-  };
-
   return (
-    <>
-      <div className="app" style={bodyTheme(theme)}>
-        <header className="appHeader" style={headerTheme(theme)}>
-          <h1>
-            <span>Re</span>azy
-          </h1>
-          <h2>
-            converts text to "Bionic Reading" text using some simple javascript
-          </h2>
-          <div id="themeButtons">
-            <p>select your flavor </p>
-            <button onClick={setThemeChocolate}>Chocolate 🍫</button>
-            <button onClick={setThemeMint}>Mint🍃</button>
-            <button onClick={setThemeStrawberry}>Strawberry 🍓</button>
-          </div>
-        </header>
-        <div className="wrapper">
-          <p className="outputText">{outputText}</p>
+    <div data-theme={theme} className="app theme-background ">
+      <header className="header theme-component">
+        <h1 className="header__title">
+          <span className="header__title--highlight">Re</span>azy
+        </h1>
+        <h2 className="header__description">
+          converts text to "Bionic Reading" text using some simple javascript
+        </h2>
+      </header>
+      <div className="content theme-background">
+        <p className="output-text">{outputText}</p>
 
-          <form className="form" onSubmit={handleSubmit}>
-            <textarea
-              className="textArea"
-              value={inputText}
-              onChange={handleInput}
-            />
-            <button className="button" style={buttonTheme(theme)}>
-              Convert Text
-            </button>
-          </form>
-        </div>
+        <form className="form" onSubmit={handleSubmit}>
+          <textarea
+            className="form__textArea"
+            value={inputText}
+            onChange={handleInput}
+          />
+          <button className="form__button theme-component">Convert Text</button>
+        </form>
       </div>
-    </>
+      <div className="footer theme-component">
+        <div className="theme-buttons">
+          <p>select your flavor </p>
+          <button onClick={() => setTheme(THEME_NAMES.CHOCOLATE)}>
+            Chocolate 🍫
+          </button>
+          <button onClick={() => setTheme(THEME_NAMES.MINT)}>Mint🍃</button>
+          <button onClick={() => setTheme(THEME_NAMES.STRAWBERRY)}>
+            Strawberry 🍓
+          </button>
+        </div>
+        <h1 className="footer__credit">by Iwan Traeger-Payne</h1>
+      </div>
+    </div>
   );
 }
 
